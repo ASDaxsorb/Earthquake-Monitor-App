693a98fb8517a030b33c9fc99d3414cf9e9ac058
Axell Solis <axellsolisrubio@gmail.com>
2021-08-11 12:02:23 -0500

    Implemented database entity

---
+++ app/src/main/java/com/axellsolis/earthquakemonitor/data/entity/EarthquakeEntity.kt
@@ -0,0 +1,39 @@
+package com.axellsolis.earthquakemonitor.data.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.axellsolis.earthquakemonitor.data.model.Earthquake
+import com.axellsolis.earthquakemonitor.data.model.Geometry
+import com.axellsolis.earthquakemonitor.data.model.Properties
+
+@Entity(tableName = "earthquake")
+data class EarthquakeEntity(
+    @ColumnInfo(name = "id")
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "place")
+    val place: String,
+    @ColumnInfo(name = "magnitude")
+    val magnitude: Double,
+    @ColumnInfo(name = "time")
+    val time: Long,
+    @ColumnInfo(name = "latitude")
+    val latitude: Double,
+    @ColumnInfo(name = "longitude")
+    val longitude: Double,
+    @ColumnInfo(name = "depth")
+    val depth: Double
+) {
+    fun toModel(): Earthquake {
+        val coordinates: List<Double> = listOf(longitude, magnitude, depth)
+        val properties = Properties(magnitude, place, time)
+        val geometry = Geometry(coordinates)
+
+        return Earthquake(
+            id.toString(),
+            properties = properties,
+            geometry = geometry
+        )
+    }
+}
--- app/src/main/java/com/axellsolis/earthquakemonitor/data/model/Earthquake.kt
+++ app/src/main/java/com/axellsolis/earthquakemonitor/data/model/Earthquake.kt
@@ -1,5 +1,6 @@
 package com.axellsolis.earthquakemonitor.data.model
 
+import com.axellsolis.earthquakemonitor.data.entity.EarthquakeEntity
 import com.google.gson.annotations.SerializedName
 
 data class Earthquake(
@@ -9,4 +10,13 @@ data class Earthquake(
     val geometry: Geometry,
     @SerializedName("properties")
     val properties: Properties,
-)
+) {
+    fun toEntity(): EarthquakeEntity = EarthquakeEntity(
+        place = properties.place,
+        magnitude = properties.magnitude,
+        time = properties.time,
+        latitude = geometry.coordinates[1],
+        longitude = geometry.coordinates[0],
+        depth = geometry.coordinates[2]
+    )
+}
